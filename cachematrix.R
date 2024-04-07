fd74f1c78fcf195ef8f54ae37c11f480de515460
alice2chen <158802663+alice2chen@users.noreply.github.com>
2024-04-07 14:30:15 +0800

    Update cachematrix.R
    update by adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this is the Week 3 assignment and also the 1st to use Github
+## functions do: to make the matrix and put into cache for future use in order to save resource
 
-## Write a short comment describing this function
+## Write a short comment describing this function: setup this function matrix and get the results
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function: to read from cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinverse <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("to get cached data")
+    return(inv)
+  }
+  matrix_to_invert <- x$get()
+  inv <- solve(matrix_to_invert, ...)
+  x$setinverse(inv)
+  inv
 }
+
+##below is the test
+wk3_Matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+wk3_Matrix$get()
+wk3_Matrix$getinverse()
+cacheinverse(wk3_Matrix)
+cacheinverse(wk3_Matrix)
